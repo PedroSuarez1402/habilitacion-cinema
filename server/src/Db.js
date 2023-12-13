3ec9333c60b1bb9e9886295ef63651cf5a1ab4d6
Eduardo <eduardo.neo09@gmail.com>
2023-12-13 02:32:38 -0500

    Update Db.js

--- server/src/Db.js
+++ server/src/Db.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
-export const connectDb = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://root:root@cluster0.uoqvgaz.mongodb.net/cinema?retryWrites=true&w=majority');
-        console.log('MongoDB connected')
-    } catch (error) {
-        console.error(error.message)
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://<username>:<password>@<cluster>/<dbname>?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
